cc81d84eee3d1001f05e71a3981e6e66c8651224
weigenie <ieeweigen@hotmail.com>
2020-10-10 04:36:37 +0800

    minor bug fix

--- lib/components/caregiverCard.dart
+++ lib/components/caregiverCard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:team7_app/views/ClientCaregiver.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:team7_app/views/clientCaregiver.dart';
 
